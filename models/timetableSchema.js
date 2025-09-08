a6b6cadabe76e7d2cea7511d48ac97fd213d202f
JohariSahil <sahiljohari04@gmail.com>
2025-09-08 08:33:40 +0530

    added timetable

---
+++ models/timetableSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const periodSchema = new mongoose.Schema({
+  day: {
+    type: String,
+    enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    required: true,
+  },
+  periodNumber: { type: Number, required: true }, // e.g., 1st, 2nd, 3rd period
+  classId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class",
+    required: true,
+  },
+  subjectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject",
+    required: true,
+  },
+  teacherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Teacher",
+    required: true,
+  },
+});
+
+periodSchema.index({ day: 1, periodNumber: 1, teacherId: 1 }, { unique: true });
+// ensures no teacher conflict in same period
+
+export const Period = mongoose.model("Period", periodSchema);
