d3f02fba1290a50c3ead2b7e451c0dd267cd0506
Sahil Johari <sahiljohari04@gmail.com>
2025-11-24 11:24:37 +0530

    commented server alive every 5 mins

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ import axios from "axios";
 const BASE_URL = process.env.BASE_URL || `https://school-erp-backend-mjwc.onrender.com`;
 
 // Keep Alive Function
-const keepServerAlive = () => {
-  const HEALTH_URL = `${BASE_URL}/api/v1/health`;
+// const keepServerAlive = () => {
+//   const HEALTH_URL = `${BASE_URL}/api/v1/health`;
 
-  const ping = async () => {
-    try {
-      const res = await axios.get(HEALTH_URL);
-      console.log(`[KeepAlive] ✅ Ping successful: ${res.data.message} at ${new Date().toISOString()}`);
-    } catch (err) {
-      console.error(`[KeepAlive] ❌ Ping failed: ${err.response?.status || err.message}`);
-    }
-  };
+//   const ping = async () => {
+//     try {
+//       const res = await axios.get(HEALTH_URL);
+//       console.log(`[KeepAlive] ✅ Ping successful: ${res.data.message} at ${new Date().toISOString()}`);
+//     } catch (err) {
+//       console.error(`[KeepAlive] ❌ Ping failed: ${err.response?.status || err.message}`);
+//     }
+//   };
 
-  // Wait 10s before first ping (let server boot fully)
-  setTimeout(() => {
-    ping();
-    setInterval(ping, 5 * 60 * 1000); // every 5 minutes
-  }, 10 * 1000);
-};
+//   // Wait 10s before first ping (let server boot fully)
+//   setTimeout(() => {
+//     ping();
+//     setInterval(ping, 5 * 60 * 1000); // every 5 minutes
+//   }, 10 * 1000);
+// };
 
 app.listen(process.env.PORT, () => {
     console.log( `Server listening on port ${process.env.PORT} `);
-    keepServerAlive();
+    // keepServerAlive();
 });
